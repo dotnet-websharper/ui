4b60a0fd76d99645ff00a8020329fe50471f0272
Simon Fowler <simon@simonjf.com>
2014-06-10 18:53:33 +0200

    - Some edits to RVar and RView - IVars now use a safer implementation of IVar.First, thanks to Anton, but this doesn't work with WebSharper just yet since it doesn't compile in a [<JavaScript>] block - Created a separate project for the console test

--- DataflowAttempt1/DataFlowAttempt1/IVar.fs
+++ DataflowAttempt1/DataFlowAttempt1/IVar.fs
@@ -1,57 +1,64 @@
 ﻿module IntelliFactory.WebSharper.UI.Next.IVar
-
+ 
 open System
 open System.Collections.Generic
 
-/// Implementation of the IVar abstraction from CML.
-/// We have two operations: Get and Put.
-/// We have two states -- empty and full -- each with different semantics for
-/// the operations.
+open IntelliFactory.WebSharper
 
-/// In the Empty state, a Put transitions us into the Full state, and puts a 
-/// given value into the mutable reference cell. Get blocks until we have a
-/// value.
-/// In the Full state, a Put is erroneous. Get doesn't trigger a state transition
-/// and simply returns the value.
-
-type State<'T> = | Empty of LinkedList<'T -> unit>
+[<JavaScript>]
+type State<'T> = | Empty of LinkedList<'T -> unit> 
                  | Full of 'T
 
-type IVar<'T> = { mutable State : State<'T> }
-
-let Create () = { State = Empty (LinkedList ()) }
-
-/// Gets the value of the IVar when it becomes available
-/// Async<'T>
-let Get iv = 
-    match iv.State with
-    | Empty waiting -> 
-        // Add callback to continuation list
-        Async.FromContinuations 
-            (fun (callback, _, _) -> waiting.AddFirst callback |> ignore)
-    | Full v -> async { return v }
-        
+[<JavaScript>]
+type IVar<'T> = { mutable State : State<'T>; Root : obj }
+ 
+[<JavaScript>]
+let Create () = { State = Empty (LinkedList ()); Root = obj () }
+ 
+[<JavaScript>]
+let When (v: IVar<'T>) (k: 'T -> unit) =
+    let v =
+        lock v.Root <| fun () ->
+            match v.State with
+            | Empty cc -> cc.AddFirst(k) |> ignore; None
+            | Full r -> Some r
+    Option.iter k v
+ 
+/// Gets the value of the IVar when it becomes available.
+[<JavaScript>]
+let Get v =
+    Async.FromContinuations (fun (k, _, _) -> When v k)
+ 
 /// Puts a value into the IVar if uninitialised, then notifies all waiting
-/// processes. 
-/// This should not be called if the IVar is already set.
-let Put (iv : IVar<'T>) (new_val : 'T) =
-    match iv.State with
-    | Empty waiting ->
-        let new_st = Full new_val
-        iv.State <- new_st
-        for p in waiting do 
+/// processes. This should not be called if the IVar is already set.
+[<JavaScript>]
+let Put var value =
+    let waiting =
+        lock var.Root <| fun () ->
+            match var.State with
+            | Empty waiting ->
+                var.State <- Full value
+                Some waiting
+            | Full _ ->
+                None
+    match waiting with
+    | None -> failwith "IVar.Put: already set"
+    | Some waiting ->
+        for p in waiting do
             // Return the value to all of the tasks waiting for it
-            Async.Start ( async { return p new_val } )
-        waiting.Clear ()
-    | Full _ -> failwith "Cannot mutate already-set IVar" // Perhaps a tad drastic
-    
-/// Waits for one of the IVars to have a value, and returns the first that is
-/// available.
-let First iv1 iv2 =
- //   let a_iv1 = Get iv1 
-   // let a_iv2 = Get iv2
-    async {
-        let res = System.Threading.Tasks.Task.WhenAny [Get iv1 |> Async.StartAsTask ; Get iv2 |> Async.StartAsTask ]
-        return res.Result.Result
-    }
-    // async { return! Get iv2 }
+            Async.Start <| async { return p value }
+ 
+/// Waits on two IVars, and returns the first one which fires
+[<JavaScript>]
+let First a b =
+    let r = Create ()
+    let root = obj ()
+    let fired = ref false
+    let k x =
+        lock root <| fun () ->
+            if not !fired then
+                fired := true
+                Put r x
+    When a k
+    When b k
+    r
