148dc782ac652dfff11bf3c3b0b556573a28f56b
Anton Tayanovskyy (t0yv0) <anton.tayanovskyy@gmail.com>
2014-07-23 15:54:56 -0400

    Classes instead of modules for Flow

--- src/WebSharper.UI.Next/Flow.fs
+++ src/WebSharper.UI.Next/Flow.fs
@@ -11,16 +11,16 @@
 
 namespace IntelliFactory.WebSharper.UI.Next
 
-open IntelliFactory.WebSharper
 type Flow<'T> =
     {
         Render : Var<Doc> -> ('T -> unit) -> unit
     }
 
 [<JavaScript>]
-module Flow =
+[<Sealed>]
+type Flow =
 
-    let Map f x =
+    static member Map f x =
         { Render = fun var cont -> x.Render var (fun r -> (f r) |> cont) }
 
     // "Unwrap" the value from the flowlet, use it as an argument to the
@@ -29,27 +29,31 @@ module Flow =
     // Semantically, what we're doing here is running the form (or other
     // input mechanism, but let's stick with thinking about forms), getting
     // the result, and then using this as an input to the continuation.
-    let Bind m k  =
+    static member Bind m k  =
         { Render = fun var cont -> m.Render var (fun r -> (k r).Render var cont) }
 
-    let Return x =
+    static member Return x =
         { Render = fun var cont -> cont x }
 
-    let Embed fl =
+    static member Embed fl =
         let var = Var.Create Doc.Empty
         fl.Render var ignore
         Doc.EmbedView var.View
 
-    let Define f =
+    static member Define f =
         { Render = fun var cont -> Var.Set var (f cont) }
 
-    let Static doc =
-        { Render = fun var cont -> Var.Set var doc ; cont () }
+    static member Static doc =
+        { Render = fun var cont -> Var.Set var doc; cont () }
 
-    [<Sealed>]
-    type FlowBuilder() =
-        member x.Bind(comp, func) = Bind comp func
-        member x.Return(value) = Return value
-        member x.ReturnFrom(wrappedVal : Flow<'A>) = wrappedVal
+[<JavaScript>]
+[<Sealed>]
+type FlowBuilder() =
+    member x.Bind(comp, func) = Flow.Bind comp func
+    member x.Return(value) = Flow.Return value
+    member x.ReturnFrom(inner: Flow<'A>) = inner
 
-    let Do = FlowBuilder()
+type Flow with
+
+    static member Do =
+        FlowBuilder()
