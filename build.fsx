13bf4b5b7fc7757d6172d664e54bcb33c1da908e
Andr?s Jank? <andras.janko@gmail.com>
2014-09-23 16:45:59 +0200

    removing tp, moved to WebSharper.UI.Next.Templating

--- build.fsx
+++ build.fsx
@@ -9,16 +9,6 @@ let main =
     bt.WebSharper.Library("WebSharper.UI.Next")
         .SourcesFromProject()
 
-let tmpl =
-    bt.WebSharper.Library("WebSharper.UI.Next.Templating")
-        .SourcesFromProject()
-        .References(fun r ->
-            [
-                r.Project main
-                r.Assembly "System.Xml"
-                r.Assembly "System.Xml.Linq"
-            ])
-
 let mainNuGet =
     bt.NuGet.CreatePackage()
         .Configure(fun c ->
@@ -30,23 +20,8 @@ let mainNuGet =
                 RequiresLicenseAcceptance = false })
         .Add(main)
 
-let tmplNuGet = 
-    bt.PackageId("WebSharper.UI.Next.Templating", "0.1").NuGet.CreatePackage()
-        .Configure(fun c ->
-            { c with
-                Title = Some "WebSharper.UI.Next.Templating"
-                LicenseUrl = Some "http://websharper.com/licensing"
-                ProjectUrl = Some "https://github.com/intellifactory/websharper.ui.next"
-                Description = "Type provider for WebSharper templating"
-                RequiresLicenseAcceptance = false })
-        .Add(tmpl)
-        .AddPackage(mainNuGet)
-
 bt.Solution [
     main
-    tmpl
-
     mainNuGet
-    tmplNuGet
 ]
 |> bt.Dispatch
