a2d901cfce2a3695323f439d72f9afe602021933
András Jankó <andras.janko@gmail.com>
2015-03-31 19:22:30 +0200

    remove alpha tag

--- build.fsx
+++ build.fsx
@@ -3,7 +3,7 @@
 open IntelliFactory.Build
 
 let bt =
-    BuildTool().PackageId("WebSharper.UI.Next", "3.0-alpha")
+    BuildTool().PackageId("WebSharper.UI.Next", "3.0")
     |> fun bt -> bt.WithFramework(bt.Framework.Net40)
 
 let main =
