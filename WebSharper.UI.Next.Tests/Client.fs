b53557272f247927950474d78da5e9354064a5dd
Loic Denuziere <loic.denuziere@intellifactory.com>
2015-04-27 19:36:07 +0200

    Update test to include ConvertSeqBy and GetItemPartRef

--- WebSharper.UI.Next.Tests/Client.fs
+++ WebSharper.UI.Next.Tests/Client.fs
@@ -13,34 +13,45 @@ module Client =
 
     type MyTemplate = Template<TemplateHtmlPath> 
 
-    type Item = { name: string; description: string }
+    type Item =
+        { name: string; description: string }
+
+        static member Key i = i.name
 
     let Main =
         let myItems =
-          ListModel.FromSeq [
-            { name = "Item1"; description = "Description of Item1" }
-            { name = "Item2"; description = "Description of Item2" }
-          ]
- 
-        let title = View.Const "Starting title"
-        let var = Var.Create ""
-        let btnVar = Var.Create ()
- 
-        let doc =
-            MyTemplate.Doc(
-                Title = title,
-                ListContainer =
-                    (ListModel.View myItems |> Doc.Convert (fun item ->
-                        MyTemplate.ListItem.Doc(
-                            Name = View.Const item.name,
-                            Description = View.Const item.description,
-                            FontStyle = View.Const "normal",
-                            FontWeight = View.Const "bold")
-                    )),
-                MyInput = var,
-                MyInputView = View.SnapshotOn "" btnVar.View var.View,
-                MyCallback = (fun e -> Var.Set btnVar ())
-            )
+            ListModel.Create Item.Key [
+                { name = "John"; description = "He's a cool guy." }
+                { name = "Jack"; description = "I like him too." }
+            ]
+        let newItemName = Var.Create ""
+        let newItemDescription = Var.Create ""
 
-        doc |> Doc.RunById "main"
-        Console.Log("Running JavaScript Entry Point..")
+        MyTemplate.Doc(
+            Title = View.Const "A bunch of people",
+            ListContainer =
+                (myItems.View |> Doc.ConvertSeqBy Item.Key (fun key item ->
+                    let rvIsEditVisible = Var.Create false
+                    let rvSaveEdit = Var.Create ()
+                    MyTemplate.ListItem.Doc(
+                        Name = (item |> View.Map (fun i -> i.name)),
+                        Description = (item |> View.Map (fun i -> i.description)),
+                        ToggleEdit = (fun _ -> Var.Update rvIsEditVisible not),
+                        EditVisible = (rvIsEditVisible.View |> View.Map (fun v ->
+                            if v then "block" else "none")),
+                        EditButtonText = (rvIsEditVisible.View |> View.Map (fun v ->
+                            if v then "Done" else "Edit")),
+                        EditBox = (key |> myItems.GetItemPartRef
+                            (fun i -> i.description)
+                            (fun i v -> { i with description = v })),
+                        RemoveItem = (fun _ -> myItems.RemoveByKey key)
+                    )
+                )),
+            NewItemName = newItemName,
+            NewItemDescription = newItemDescription,
+            AddNewItem = (fun _ ->
+                myItems.Add({ name = newItemName.Value; description = newItemDescription.Value })
+                newItemName.Value <- ""
+                newItemDescription.Value <- "")
+        )
+        |> Doc.RunById "main"
