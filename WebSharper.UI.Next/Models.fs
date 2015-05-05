dec0aca7ccbe2420c18c6d6729e31fc2a6209a24
István Gansperger <gansperger@gmail.com>
2015-05-05 12:15:39 +0200

    Added storage update for UpdateAll

--- WebSharper.UI.Next/Models.fs
+++ WebSharper.UI.Next/Models.fs
@@ -203,7 +203,7 @@ type ListModel<'Key,'T> with
         Var.Update m.Var <| fun a ->
             a |> Array.iteri (fun i x ->
                 fn x |> Option.iter (fun y -> a.[i] <- y))
-            a
+            m.Storage.Set a
 
     member m.UpdateBy fn key =
         let v = m.Var.Value
