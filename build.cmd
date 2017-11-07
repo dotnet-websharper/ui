815773d9fa40ef4eb425030af92c8c3b734ee247
András Jankó <andras.janko@gmail.com>
2017-11-07 14:21:19 +0100

    fixes for fake/paket conversion

--- build.cmd
+++ build.cmd
@@ -4,9 +4,13 @@ set PATH=%GitToolPath%;%PATH%
 
 cls
 
-dotnet restore
+.paket\paket.exe restore --touch-affected-refs
 if errorlevel 1 (
   exit /b %errorlevel%
+)
+
+if "%VisualStudioVersion%"=="" (
+  set VisualStudioVersion=15.0
 )
 
 packages\build\FAKE\tools\FAKE.exe build.fsx %*
