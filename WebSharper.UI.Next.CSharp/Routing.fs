393a9c09b36416ef888adcc181b5c942973d7810
András Jankó <andras.janko@gmail.com>
2017-11-15 18:33:50 +0100

    fix for moving Sitelets attributes

--- WebSharper.UI.Next.CSharp/Routing.fs
+++ WebSharper.UI.Next.CSharp/Routing.fs
@@ -44,9 +44,6 @@ module private Internals =
             )
         defaultArg o (^T : (member Name : string)(x))
 
-
-type EndPointAttribute = Sitelets.EndPointAttribute
-
 [<JavaScript>]
 type RouteMapBuilder() =
 
