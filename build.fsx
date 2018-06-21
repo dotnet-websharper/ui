bfeb5bdccd1ba0f55386ecfa8be92e0a63ce59d1
Loic Denuziere <loic@denuziere.net>
2018-06-21 11:59:52 +0200

    Build MSBuild task separately + add generated files to Compile

--- build.fsx
+++ build.fsx
@@ -2,9 +2,16 @@
 open Fake
 open WebSharper.Fake
 
-let targets =
-    WSTargets.Default (fun () -> GetSemVerOf "WebSharper" |> ComputeVersion)
-    |> MakeTargets
+let targets = MakeTargets {
+    WSTargets.Default (fun () -> GetSemVerOf "WebSharper" |> ComputeVersion) with
+        BuildAction =
+            BuildAction.Projects [
+                // Build the build task first, so that it exists
+                // and can be loaded by the test projects in the sln.
+                "WebSharper.UI.CSharp.Templating.Build/WebSharper.UI.CSharp.Templating.Build.fsproj"
+                "WebSharper.UI.sln"
+            ]
+}
 
 Target "Build" DoNothing
 targets.BuildDebug ==> "Build"
