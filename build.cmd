ebb8e383e3c7aadeb331c5252ac5b39c3538579e
András Jankó <andras.janko@gmail.com>
2020-03-02 17:21:29 +0100

    move to FAKE5

--- build.cmd
+++ build.cmd
@@ -1,12 +1,8 @@
 @echo off
 setlocal
 
-if not "%WsUpdate%"=="" (
-  .paket\paket.exe update -g wsbuild --no-install
-  if errorlevel 1 exit /b %errorlevel%
-)
-
-.paket\paket.exe restore
+dotnet restore
 if errorlevel 1 exit /b %errorlevel%
 
-call paket-files\wsbuild\github.com\dotnet-websharper\build-script\WebSharper.Fake.cmd %*
+call paket-files\wsbuild\github.com\dotnet-websharper\build-script\update.cmd
+call paket-files\wsbuild\github.com\dotnet-websharper\build-script\build.cmd %*
--- build.fsx
+++ build.fsx
@@ -1,22 +1,14 @@
+#r "paket: groupref build //"
 #load "paket-files/wsbuild/github.com/dotnet-websharper/build-script/WebSharper.Fake.fsx"
-open Fake
 open WebSharper.Fake
 
-let targets = MakeTargets {
-    WSTargets.Default (fun () -> GetSemVerOf "WebSharper" |> ComputeVersion) with
-        BuildAction =
-            BuildAction.Projects [
-                // Build the build task first, so that it exists
-                // and can be loaded by the test projects in the sln.
-                "WebSharper.UI.CSharp.Templating.Build/WebSharper.UI.CSharp.Templating.Build.fsproj"
-                "WebSharper.UI.sln"
-            ]
-}
+let WithProjects projects args =
+    { args with BuildAction = Projects projects }
 
-Target "Build" DoNothing
-targets.BuildDebug ==> "Build"
-
-Target "CI-Release" DoNothing
-targets.Publish ==> "CI-Release"
-
-RunTargetOrDefault "Build"
+LazyVersionFrom "WebSharper" |> WSTargets.Default
+|> WithProjects [
+    "WebSharper.UI.CSharp.Templating.Build/WebSharper.UI.CSharp.Templating.Build.fsproj"
+    "WebSharper.UI.sln"
+]
+|> MakeTargets
+|> RunTargets
