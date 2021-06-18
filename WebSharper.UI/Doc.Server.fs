97fc893ea2f87036e2aac3547b3067a06b0cb310
András Jankó <andras.janko@gmail.com>
2021-06-18 22:35:09 +0200

    do not close stream on Page write

--- WebSharper.UI/Doc.Server.fs
+++ WebSharper.UI/Doc.Server.fs
@@ -41,7 +41,8 @@ type Content =
                 Status = Http.Status.Ok,
                 Headers = [Http.Header.Custom "Content-Type" "text/html; charset=utf-8"],
                 WriteBody = fun s ->
-                    use w = new System.IO.StreamWriter(s, Text.Encoding.UTF8)
+                    use w = new System.IO.StreamWriter(s, Text.Encoding.UTF8, 1024, leaveOpen = true)
+                    w.AutoFlush <- AutoFlushPageWriter
                     use w = new HtmlTextWriter(w)
                     w.WriteLine("<!DOCTYPE html>")
                     doc.Write(ctx, w, true)
