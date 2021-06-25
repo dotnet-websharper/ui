8869449895c45efa46df60da075bca6e42f2e60a
Garai Márton <garaimarton.mail@gmail.com>
2021-06-25 15:41:59 +0200

    Upgrade the test projects

---
+++ WebSharper.UI.CSharp.Tests/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WebSharper.AspNetCore;
+
+namespace WebSharper.UI.CSharp.Tests
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment()) { app.UseDeveloperExceptionPage(); }
+
+            app.UseDefaultFiles()
+                .UseStaticFiles()
+                .UseWebSharper(builder => builder.UseSitelets(false))
+                .Run(context => {
+                    context.Response.StatusCode = 404;
+                    return context.Response.WriteAsync("Page not found");
+                   });
+        }
+
+        public static void Main(string[] args)
+        {
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build()
+                .Run();
+        }
+    }
+}
---
+++ WebSharper.UI.Templating.ServerSide.Tests/Startup.fs
@@ -0,0 +1,57 @@
+﻿// $begin{copyright}
+//
+// This file is part of WebSharper
+//
+// Copyright (c) 2008-2018 IntelliFactory
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you
+// may not use this file except in compliance with the License.  You may
+// obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.  See the License for the specific language governing
+// permissions and limitations under the License.
+//
+// $end{copyright}
+namespace WebSharper.UI.ServerSide.Tests
+
+open System
+open Microsoft.AspNetCore
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+open WebSharper.AspNetCore
+
+type Startup() =
+
+    member this.ConfigureServices(services: IServiceCollection) =
+        services.AddSitelet<Main>()
+        |> ignore
+
+    member this.Configure(app: IApplicationBuilder, env: IWebHostEnvironment) =
+        if env.IsDevelopment() then app.UseDeveloperExceptionPage() |> ignore
+
+        app.UseDefaultFiles()
+            .UseStaticFiles()
+            .UseWebSharper()
+            .Run(fun context ->
+                context.Response.StatusCode <- 404
+                context.Response.WriteAsync("Page not found"))
+
+module Program =
+
+    [<EntryPoint>]
+    let main args =
+        WebHost
+            .CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .Build()
+            .Run()
+        0
