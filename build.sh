85e865b75b9d151c183201ccc706695819cb2fcf
András Jankó <andras.janko@gmail.com>
2021-06-30 16:25:33 +0200

    update build-script first

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
     
 dotnet tool restore
+dotnet paket update -g wsbuild --no-install
+    
+. paket-files/wsbuild/github.com/dotnet-websharper/build-script/update.sh
+
 dotnet paket restore
 dotnet restore
     
-. paket-files/wsbuild/github.com/dotnet-websharper/build-script/update.sh
 . paket-files/wsbuild/github.com/dotnet-websharper/build-script/build.sh "$@"
