4e08385fe3ad2731669af7688151d1af03d8fbdf
jooseppi12 <urijozsef@europe.com>
2021-09-08 17:35:27 +0200

    Fix issue with build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,4 @@ dotnet tool restore
 
 dotnet paket update -g wsbuild --no-install
 
-export DOTNETSOLUTION="WebSharper.Compiler.sln;WebSharper.sln"
 . paket-files/wsbuild/github.com/dotnet-websharper/build-script/build.sh "$@"
