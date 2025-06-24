e5e0ccee1a7e84684db1f7b9458b2997ab25e5a1
András Jankó <andras.janko@gmail.com>
2025-06-24 17:27:20 +0200

    build: stop service on CI-Commit

--- build.fsx
+++ build.fsx
@@ -9,6 +9,7 @@
 #r "nuget: MSBuild.StructuredLogger"
 
 open Fake.Core
+open System.Diagnostics
 
 System.Environment.GetCommandLineArgs()
 |> Array.skip 2 // skip fsi.exe; build.fsx
@@ -20,23 +21,42 @@ System.Environment.GetCommandLineArgs()
 #load "paket-files/wsbuild/github.com/dotnet-websharper/build-script/WebSharper.Fake.fsx"
 open WebSharper.Fake
 open Fake.DotNet
+open Fake.Core.TargetOperators
 
 let WithProjects projects args =
     { args with BuildAction = Projects projects }
 
-LazyVersionFrom "WebSharper" |> WSTargets.Default
-|> fun args ->
-    { args with
-        Attributes = [
-            AssemblyInfo.Company "IntelliFactory"
-            AssemblyInfo.Copyright "(c) IntelliFactory 2023"
-            AssemblyInfo.Title "https://github.com/dotnet-websharper/ui"
-            AssemblyInfo.Product "WebSharper UI"
-        ]
-    }
-|> WithProjects [
-    "WebSharper.UI.CSharp.Templating.Build/WebSharper.UI.CSharp.Templating.Build.fsproj"
-    "WebSharper.UI.sln"
-]
-|> MakeTargets
-|> RunTargets
+let targets = 
+    LazyVersionFrom "WebSharper" |> WSTargets.Default
+    |> fun args ->
+        { args with
+            Attributes = [
+                AssemblyInfo.Company "IntelliFactory"
+                AssemblyInfo.Copyright "(c) IntelliFactory 2023"
+                AssemblyInfo.Title "https://github.com/dotnet-websharper/ui"
+                AssemblyInfo.Product "WebSharper UI"
+            ]
+        }
+    |> WithProjects [
+        "WebSharper.UI.CSharp.Templating.Build/WebSharper.UI.CSharp.Templating.Build.fsproj"
+        "WebSharper.UI.sln"
+    ]
+    |> MakeTargets
+
+Target.create "WS-StopAll" <| fun _ ->
+    try
+        Process.GetProcessesByName("wsfscservice")
+        |> Array.iter (fun x -> 
+            x.Kill()
+        )
+        |> ignore
+    with
+    | _ -> ()
+
+"WS-StopAll"
+    ?=> "WS-Clean"
+
+"WS-StopAll"
+    ==> "CI-Commit"
+
+RunTargets targets
